474360ea39d5d6908edbd51c083e550ab22236de
moralapablo <moralapablo@gmail.com>
2021-01-16 12:44:26 +0100

    Defined new function to plot the surfaces together.

---
+++ functions/plotSurfaceComparison.R
@@ -0,0 +1,44 @@
+plotSurfaceComparison <- function(x1,x2,y,train_x1,train_x2,prediction,x1_big,x2_big,y_big, my_breaks,my_theta,my_phi){
+  # Polynomial in input range
+  par(mfrow=c(1,2))
+  plot3D::persp3D(x1,
+                  x2,
+                  y,
+                  theta = my_theta,
+                  breaks = my_breaks,
+                  colkey = FALSE,
+                  phi = my_phi,
+                  xlab = "x1", ylab = "x2",
+                  zlab = "y"
+  )
+  plot3D::points3D(train_x1,
+                   train_x2,
+                   prediction,
+                   colvar = NULL,
+                   col = "black",
+                   bg = "black",
+                   size = 30,
+                   pch = 23,
+                   add = TRUE
+  )
+  # Polynomial Extended
+  plot3D::persp3D(x1_big,
+                  x2_big,
+                  y_big,
+                  theta = my_theta,
+                  breaks = my_breaks,
+                  phi = my_phi,
+                  xlab = "x1", ylab = "x2",
+                  zlab = "y"
+  )
+  plot3D::points3D(train_x1,
+                   train_x2,
+                   prediction,
+                   colvar = NULL,
+                   col = "black",
+                   bg = "black",
+                   size = 30,
+                   pch = 23,
+                   add = TRUE
+  )
+}
